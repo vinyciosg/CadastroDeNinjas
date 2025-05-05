d91e39913a14c4ad197a296fc739bb2413606b0e
vinycios <vinyciosofficial@gmail.com>
2025-05-05 20:28:26 -0300

    Cria uma entidade no banco de dados

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,48 @@
+package dev.java10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_Cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String idade;
+    String email;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
